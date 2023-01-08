ab1995f7efbe51ac7cc2acd8a4909267f64f6f45
CopperFull <nguyenday3332@gmail.com>
2023-01-08 20:27:50 +0700

    mv change volume onloadtart

--- src/Component/MVview/Video/index.js
+++ src/Component/MVview/Video/index.js
@@ -1,7 +1,15 @@
 function Video({ src, cx }) {
   return (
     <div className={cx("videowrapper")}>
-      <video className={cx("video")} src={src} controls autoPlay></video>
+      <video
+        className={cx("video")}
+        src={src}
+        controls
+        autoPlay
+        onLoadStart={(e) => {
+          e.target.volume = 0.5;
+        }}
+      ></video>
     </div>
   );
 }
